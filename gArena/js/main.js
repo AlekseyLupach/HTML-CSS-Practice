2d72eb697f39084a7aea9af8f1b94f62c8c24b96
Aliaksei <CuBbI@MacBook-Pro-Aliaksei-2.local>
2020-10-21 13:32:38 +0300

    add-adaptiv

--- gArena/js/main.js
+++ gArena/js/main.js
@@ -1,3 +1,4 @@
+// News carousel start
 $('#owl-carousel').each(function () {
 	var owl = $(this).find('.owl-carousel').owlCarousel({
 		margin: 10,
@@ -21,6 +22,8 @@ $('#owl-carousel').each(function () {
 		owl.trigger('next.owl.carousel');
 	});
 });
+// News carousel end
+
 // burger__menu start
 
 $(document).ready(function () {
@@ -59,8 +62,8 @@ $('.upbtn').on('click', function () {
 
 // smooth scrolling start
 $(window).on("load", function () {
-	$("#navigation-menu a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
-		highlightSelector: "#navigation-menu a"
+	$("#header__li a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
+		highlightSelector: "#header__li a"
 	});
 	$("#header__text-button a,a[href='#top'],a[rel='m_PageScroll2id']").mPageScroll2id({
 		highlightSelector: "#header__text-button a"
@@ -147,3 +150,49 @@ $(document).ready(function () { $("video").prop('muted', true); })
 new WOW().init();
 // wow end
 
+// Hover h3 color for Equipment pic start
+var picDiv = document.querySelectorAll('.equipment__wrapper-pic')[0];
+picDiv.addEventListener('mouseover', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[0];
+	h3.style.color = "#007bff";
+	h3.classList.add('active');
+});
+picDiv.addEventListener('mouseleave', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[0];
+	h3.style.color = null;
+	h3.classList.remove('active');
+});
+var picDiv = document.querySelectorAll('.equipment__wrapper-pic')[1];
+picDiv.addEventListener('mouseover', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[1];
+	h3.style.color = "#007bff";
+	h3.classList.add('active');
+});
+picDiv.addEventListener('mouseleave', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[1];
+	h3.style.color = null;
+	h3.classList.remove('active');
+});
+var picDiv = document.querySelectorAll('.equipment__wrapper-pic')[2];
+picDiv.addEventListener('mouseover', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[2];
+	h3.style.color = "#007bff";
+	h3.classList.add('active');
+});
+picDiv.addEventListener('mouseleave', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[2];
+	h3.style.color = null;
+	h3.classList.remove('active');
+});
+var picDiv = document.querySelectorAll('.equipment__wrapper-pic')[3];
+picDiv.addEventListener('mouseover', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[3];
+	h3.style.color = "#007bff";
+	h3.classList.add('active');
+});
+picDiv.addEventListener('mouseleave', function (event) {
+	var h3 = document.querySelectorAll('.equipment__card-title')[3];
+	h3.style.color = null;
+	h3.classList.remove('active');
+});
+// Hover h3 color for Equipment pic end
